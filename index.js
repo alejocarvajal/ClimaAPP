67e227f2583a9a5e1ffcf2ec5f0ef8debb488019
Alejandro Carvajal <alejo.carvajal03@gmail.com>
2022-02-01 17:48:35 -0500

    Se implementa axios para el llamado del API de ciudades

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const {leerInput, inquirerMenu, pausa} = require("./hekpers/inquirer");
+require('dotenv').config();
+
+const {leerInput, inquirerMenu, pausa, listadoLugares} = require("./hekpers/inquirer");
+const Busquedas = require("./models/busquedas");
 const main = async () => {
     let opt;
-
+    const busquedas = new Busquedas();
     do {
         opt = await inquirerMenu();
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+
+                //Buscar los lugares
+                const lugares = await busquedas.ciudad(lugar);
+
+                //Seleccione el lugar
+                const id = await listadoLugares(lugares);
+                const lugarSel = lugares.find(l => l.id === id);
+
+                //Clima
+
+                //Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad: ', lugarSel.nombre);
+                console.log('Lat: ', lugarSel.lat);
+                console.log('Long: ', lugarSel.lng);
+                console.log('Temperatura: ',);
+                console.log('Minima: ',);
+                console.log('Maxima: ',);
+                break;
+
+        }
         if (opt !== 0) await pausa();
     } while (opt !== 0)
 
---
+++ models/busquedas.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+
+class Busquedas {
+    historial = ['Medellin', 'Cartago', 'Cali'];
+
+    constructor() {
+
+    }
+
+    get paramsMapbox() {
+        return {
+            'access_token': process.env.MAPBOX_KEY,
+            'limit': 5,
+            'language': 'es'
+        };
+    }
+
+    async ciudad(lugar = '') {
+
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();
+            return resp.data.features.map( lugar =>({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+            }));
+        } catch (error) {
+            return [];
+        }
+    }
+
+}
+
+module.exports = Busquedas;
